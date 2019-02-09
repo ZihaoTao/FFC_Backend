6f51e796f1efa5d5ff68bd5928e5eb970a45575e
Zihao Tao <taozihao@uw.edu>
2019-02-08 23:20:37 -0800

    test

--- src/main/java/com/web/controller/protal/ProductController.java
+++ src/main/java/com/web/controller/protal/ProductController.java
@@ -1,9 +1,12 @@
 package com.web.controller.protal;
 
-import com.github.pagehelper.PageInfo;
+import com.google.gson.Gson;
 import com.web.common.ServerResponse;
 import com.web.service.IProductService;
+import com.web.service.impl.CategoryServiceImpl;
 import com.web.vo.ProductDetailVo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +16,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 /**
  * Created by tino on 10/16/18.
  */
-
 @Controller
 @RequestMapping("/product/")
 public class ProductController {
+
+    private Logger logger = LoggerFactory.getLogger(CategoryServiceImpl.class);
 
     @Autowired
     private IProductService iProductService;
@@ -29,19 +33,25 @@ public class ProductController {
 
     @RequestMapping("list.do")
     @ResponseBody
-    public ServerResponse<PageInfo> list(@RequestParam(value = "keyword", required = false) String keyword,
-                                         @RequestParam(value = "categoryId", required = false) Integer categoryId,
-                                         @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
-                                         @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
-                                         @RequestParam(value = "orderBy", defaultValue = "") String orderBy) {
-        return iProductService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy);
+    public String list(@RequestParam("callback") String callback,
+                       @RequestParam(value = "keyword", required = false) String keyword,
+                       @RequestParam(value = "categoryId", required = false) Integer categoryId,
+                       @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+                       @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
+                       @RequestParam(value = "orderBy", defaultValue = "") String orderBy) {
+        Gson gson=new Gson();
+        logger.info(callback);
+        return callback+ "(" + gson.toJson(iProductService.getProductByKeywordCategory(keyword, categoryId, pageNum, pageSize, orderBy)) + ")";
     }
 
     @RequestMapping("all.do")
     @ResponseBody
-    public ServerResponse<PageInfo> all(@RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
+    public String all(@RequestParam("callback") String callback,
+                                        @RequestParam(value = "pageNum", defaultValue = "1") int pageNum,
                                         @RequestParam(value = "pageSize", defaultValue = "10") int pageSize,
                                         @RequestParam(value = "orderBy", defaultValue = "") String orderBy) {
-        return iProductService.getAllProduct(pageNum, pageSize, orderBy);
+        Gson gson=new Gson();
+        logger.info(callback);
+        return callback+ "(" + gson.toJson(iProductService.getAllProduct(pageNum, pageSize, orderBy)) + ")";
     }
 }
